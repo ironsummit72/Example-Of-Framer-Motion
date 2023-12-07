0ebc86571d73cdcd1903c018226254a89a64c487
ironsummit72 <ironsumit72@gmail.com>
2023-12-07 22:40:32 +0530

    added Framer Motion for animation and dragibility

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,10 @@ import { SiGoogledocs } from "react-icons/si";
 import { SiMicrosoftexcel } from "react-icons/si";
 import { PiMicrosoftWordLogoFill } from "react-icons/pi";
 import { FaFile } from "react-icons/fa";
+import { motion,useAnimate } from "framer-motion"
 
 const renderSwitch=(params)=> {
+    const animate=useAnimate();
     switch (params) {
       case "xlsx":
         return <SiMicrosoftexcel size={30} color={"#339966"} />;
@@ -40,13 +42,13 @@ const renderColor=(params)=>{
       }
 }
 
-function Card({filetype='xlsxs',filename,isDownload}) {
+function Card({filetype='xlsxs',filename,isDownload,referance}) {
   return (
-    <div className="card h-96 mt-10 ml-10 w-72 bg-[#FAF6F0] rounded-2xl flex flex-col justify-between  shadow-2xl cursor-pointer">
+    <motion.div  drag whileHover={{scale:1.1}} dragConstraints={referance} whileDrag={{scale:1.2}} animate={{x: 19,y: -23,scale: 1,rotate: 360}} className="card h-96 mt-10 ml-10 w-72 bg-[#FAF6F0] rounded-2xl flex flex-col justify-between  shadow-2xl cursor-pointer">
         <div className="icon flex items-center relative left-10 h-20">{renderSwitch(filetype)}</div>
         <p className="filename relative left-4 font-semibold text-lg  w-[90%] right-5 ">{filename}</p>
         <div className={`button w-full border font-bold bg-[${renderColor(filetype)}] text-white flex justify-center p-4 relative bottom-0 rounded-br-2xl rounded-bl-2xl cursor-pointer`}>{isDownload?"Download File":"Upload File"}</div>
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,12 +1,15 @@
 import Card from "./Card"
 import Files from '../files.json'
+import { useRef } from "react"
+
 function Foreground() {
+    const referance=useRef(null);
   return (
     <>
-      <div className="absolute top-0 left-0 z-[3] w-full h-auto grid grid-cols-3 ">
-        <Card filetype="file" filename={'Upload Your File Here'}/>
+      <div ref={referance} className="absolute top-0 left-0 z-[3] w-full h-screen grid grid-cols-3 ">
+        {/* <Card referance={referance} filetype="file" filename={'Upload Your File Here'}/> */}
         {
-            Files.map((items)=><Card filename={items.filename.split('.')[0]} filetype={items.filename.split('.')[1]}/>)
+            Files.map((items,index)=><Card referance={referance} key={index} isDownload={true} filename={items.filename.split('.')[0]} filetype={items.filename.split('.')[1]}/>)
         }
     </div>
     </>
