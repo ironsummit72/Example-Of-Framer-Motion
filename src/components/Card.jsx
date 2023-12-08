bea8399e59a5e51271c4c53a99afffb79a36dd0c
ironsummit72 <ironsumit72@gmail.com>
2023-12-08 15:59:12 +0530

    colors not rendering bug fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -41,13 +41,12 @@ const renderColor=(params)=>{
         default : return 'orange'
       }
 }
-
 function Card({filetype='xlsxs',filename,isDownload,referance}) {
   return (
     <motion.div  drag whileHover={{scale:1.1}} dragConstraints={referance} whileDrag={{scale:1.2}} animate={{x: 19,y: -23,scale: 1,rotate: 360}} className="card h-96 mt-10 ml-10 w-72 bg-[#FAF6F0] rounded-2xl flex flex-col justify-between  shadow-2xl cursor-pointer">
         <div className="icon flex items-center relative left-10 h-20">{renderSwitch(filetype)}</div>
         <p className="filename relative left-4 font-semibold text-lg  w-[90%] right-5 ">{filename}</p>
-        <div className={`button w-full border font-bold bg-[${renderColor(filetype)}] text-white flex justify-center p-4 relative bottom-0 rounded-br-2xl rounded-bl-2xl cursor-pointer`}>{isDownload?"Download File":"Upload File"}</div>
+        <div className={`button w-full border font-bold  text-white flex justify-center p-4 relative bottom-0 rounded-br-2xl rounded-bl-2xl cursor-pointer bgc-${renderColor(filetype)}`}>{isDownload?"Download File":"Upload File"}</div>
     </motion.div>
   )
 }
